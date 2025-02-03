5ad8acb3397572927c97b6afe4d107015f06bb87
raphael.vazzano <raphael.vazzano@epita.fr>
2025-02-03 18:55:29 +0100

    feat(prototype): few tweaks

--- snack_attack_like/Scripts/GameManager.gd
+++ snack_attack_like/Scripts/GameManager.gd
@@ -2,15 +2,27 @@ extends Node3D
 
 # ---------- VARIABLES ---------- #
 
-var score = 0
-var life = 3
+var const_wait_time = 1.5
+
+var score
+var life
+var wait_time
 
 # ---------- FUNCTIONS ---------- #
+
+func _ready() -> void:
+	set_game_var()
 
 func _process(_delta):
 	show_mouse_cursor()
 	game_over()
-
+	calculate_wait_time()
+	
+func set_game_var():
+	score = 0
+	life = 3 
+	wait_time = const_wait_time	
+	
 # Making Cursor visible using "mouse_visible" key which is assigned in Project Settings > Input Map
 func show_mouse_cursor():
 	if Input.is_action_just_pressed("mouse_visible"):
@@ -18,10 +30,13 @@ func show_mouse_cursor():
 
 func game_over():
 	if life <= 0 :
-		life = 3
-		score = 0
+		set_game_var()
 		get_tree().reload_current_scene()
 		
+func calculate_wait_time():
+	if wait_time >= 0.5 :
+		wait_time = const_wait_time - score * 0.05 # TODO logarithmic difficulty scale
+
 func add_score():
 	score += 1
 	
--- snack_attack_like/Scripts/ObjectSpawner.gd
+++ snack_attack_like/Scripts/ObjectSpawner.gd
@@ -5,6 +5,7 @@ extends Node3D
 @export var coin_scene : PackedScene
 
 @onready var spawn_point = $SpawnPath/SpawnPoint
+@onready var spawn_clock = $SpawnTimer
 
 enum spawned_object_enum {ROLLING_M, EXPLODING_M, COIN}
 
@@ -12,19 +13,22 @@ enum spawned_object_enum {ROLLING_M, EXPLODING_M, COIN}
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_object_timer_timeout() -> void:
-	spawn_point.progress_ratio = randf()
 	var spawned_object = get_random_object_to_spawn()
+	
+	spawn_point.progress_ratio = randf()
+	
 	add_child(spawned_object)
+	
 	spawned_object.position.x = spawn_point.position.x
 	spawned_object.position.z = spawn_point.position.z
 	spawned_object.position.y = 14
 	
+	spawn_clock.wait_time = GameManager.wait_time
 		
 func get_random_object_to_spawn():
 	var rand_int = randi_range(0, 2)
